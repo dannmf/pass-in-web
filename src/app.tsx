719d163ce0af405344f437f9b24d3b812030e20b
daniel.manoel <daniellfmanoel@gmail.com>
2024-04-03 19:05:06 -0300

    git init

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <AttendeeList />
+    </div>
+  )
+}
+
+
