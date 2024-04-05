03ff23f257511a67f77b8dfbb5ab08ed5d1a97f1
daniel.manoel <daniellfmanoel@gmail.com>
2024-04-04 23:47:12 -0300

    🚀 feat: [ header, user_list ]

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
+import nlwUniteIcon from '../assets/nlw-svg-icon.svg'
+
+
 export function Header(){
     return (
-        <div>
-            header
+        <div className='flex items-center gap-5 py-2'>
+            <img src= {nlwUniteIcon} alt="Icon da NLW" />
+            <nav className='flex items-center gap-5'>
+                <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href="" className='font-medium text-sm'>Participantes</a>
+            </nav>
         </div>
     )
 }
